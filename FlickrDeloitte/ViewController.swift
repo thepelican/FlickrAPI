9293a2ee9ec80e67076ffc6d77268f57f67e9d77
Marco Prayer Galletti <marcog@thred.im>
2018-05-25 15:00:22 +1000

    Initial Commit

---
+++ FlickrDeloitte/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FlickrDeloitte
+//
+//  Created by Leonard Wu on 25/5/18.
+//  Copyright © 2018 Marco Prayer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
